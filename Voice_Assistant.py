805c9c732a8c35db600cdee711c81a9c901ce5a2
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-19 23:09:12 +0530

    Make listen function for convert audio into text and return it.

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -2,3 +2,26 @@ import speech_recognition as sr
 import pyttsx3
 import datetime
 import webbrowser
+
+recognizer = sr.Recognizer()
+
+
+engine = pyttsx3.init()
+
+def listen():
+    with sr.Microphone() as source:
+        print("Listening.........")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+        try:
+            text = recognizer.recognize_google(audio)
+            print("You said: ",text)
+            return text.lower()
+        except sr.UnknownValueError:
+            print("Could not understand audio.")
+            return ""
+        except sr.RequestError as e:
+            print(f"Error: {e}")
+            return ""
+
