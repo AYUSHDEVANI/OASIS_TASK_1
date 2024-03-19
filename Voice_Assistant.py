12112392933cc1a0d4cb6ef4d5c6a7ffd7bfd667
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-19 23:48:29 +0530

    Add command in the function handle command.

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -2,6 +2,8 @@ import speech_recognition as sr
 import pyttsx3
 import datetime
 import webbrowser
+import os
+import wikipedia
 
 recognizer = sr.Recognizer()
 
@@ -15,7 +17,7 @@ def listen():
         audio = recognizer.listen(source)
 
         try:
-            text = recognizer.recognize_google(audio)
+            text = recognizer.recognize_google(audio,  language="en-in")
             print("You said: ",text)
             return text.lower()
         except sr.UnknownValueError:
@@ -33,12 +35,37 @@ def speak(text):
 def handle_command(command):
     if "hello" in command:
         speak("Hello! How can I assist you?")
+
     elif "time" in command:
         current_time = datetime.datetime.now().strftime("%H:%M")
         speak(f"The current time is {current_time}")
+
     elif "date" in command:
         current_date = datetime.datetime.now().strftime("%Y-%m-%d")
         speak(f"Today`s date is {current_date}")
+
+    elif "wikipedia" in command:
+        speak("Searching Wikipedia...")
+        command = command.replac("wikipedia","")
+        results = wikipedia.summary(command,sentences = 5)
+        speak("According to Wikipedia")
+        print(results)
+        speak(results)
+
+    elif "open notepad" in command:
+        speak("Opening Notepad please wait a moment.")    
+        path = "C:/Windows/System32/notepad.exe"
+        os.startfile(path)
+    
+    elif "close notepad" in command:
+        speak("Closing notepad please wait.........")
+        os.system("C:/Windows/System32/taskkill.exe /F /IM notepad.exe")
+    
+    elif "open youtube" in command:
+        speak("Here you go to youtube")
+        webbrowser.open("https://www.youtube.com/")
+
+
     elif "search" in command:
         speak("What would you like to search for?")
         search_query = listen()
@@ -51,9 +78,20 @@ def handle_command(command):
     else:
         speak("Sorry, I didn`t understand that command.")
 
+def greet():
+    hour = int(datetime.datetime.now().hour)
+    print(hour)
+    if hour >= 0 and hour<12:
+        speak("Good Morning !!")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!!")
+    else:
+        speak("Good Night!!")
+    speak("Welcome, I am Your personal Google Assistant.")
 
 def main():
-    speak("Hello, How can I assist you today?")
+    # speak("Hello, How can I assist you today?")
+    greet()
 
     while True:
         command = listen()
@@ -63,4 +101,5 @@ def main():
         handle_command(command)
 
 if __name__ == "__main__":
+ 
     main()
